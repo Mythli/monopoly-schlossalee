8d72c2bd8d167fbb755fc7675c39a40d871fb523
Thomas <thief1064@gmail.com>
2013-09-30 10:36:27 +0100

    Anlegen der Klassenrümpfe der Klassen: Bank Player Participant Dice Gameboard

--- src/monopoly/GameBoard.java
+++ src/monopoly/GameBoard.java
@@ -1,5 +1,21 @@
 package monopoly;
 
+import java.util.ArrayList;
+
 public class GameBoard {
-//
+
+	private ArrayList<Player> players = new ArrayList<Player>();
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	public void addPlayer(Player player) {
+		players.add(player);
+	}
+
+	public Player getCurrentPlayer() {
+		return null;
+	}
+
 }
--- src/monopoly/Player.java
+++ src/monopoly/Player.java
@@ -2,4 +2,35 @@ package monopoly;
 
 public class Player extends Participant {
 
+	private String name;
+	private int currentMoney;
+
+	public Player(String name) {
+		this.name = name;
+		this.currentMoney = 15000;
+	}
+
+	public int getCurrentMoney() {
+		return 0;
+	}
+
+	public void giveMoneyToParticipant(Participant player, int amount) {
+
+	}
+
+	public void addMoney(int value) {
+
+	}
+
+	public Field getCurrentField() {
+		return null;
+	}
+
+	public void move(Field field) {
+
+	}
+
+	public void move(int value) {
+
+	}
 }
