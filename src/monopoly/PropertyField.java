f4af7304e06855e979e7a3786f5e1308adef0453
unknown <Lokaler Benutzer@R-166-04.Schule.107842.mts-schule.de>
2013-09-30 09:44:15 +0100

    Klassen hinzugefügt

---
+++ src/monopoly/PropertyField.java
@@ -0,0 +1,22 @@
+package monopoly;
+
+public class PropertyField {
+	
+	 private double getPrice ()
+	 {
+		 return 0.0;
+	 }
+	 private Player getOwner ()
+	 {
+		 return null;
+	 }
+	 public void onEnter ()
+	 {
+		 
+	 }
+	 public PropertyGroup getGroup ()
+	 {
+		 
+	 }
+
+}
