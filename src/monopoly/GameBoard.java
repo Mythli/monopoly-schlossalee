d8df498a204acc6df7f0084115ed50ddae25f55c
Thomas <thief1064@gmail.com>
2013-10-14 09:57:23 +0100

    Umsetzung der Würfel-Klasse in die Klasse "GameBoard" -> getDice()

--- src/monopoly/GameBoard.java
+++ src/monopoly/GameBoard.java
@@ -7,18 +7,27 @@ public class GameBoard {
 	private ArrayList<Player> players = new ArrayList<Player>();
 	private ArrayList<Field> fields = new ArrayList<Field>();
 	private int currentPlayer;
-	private int lastRoll;
-		
+	private Dice dice;
+
+	/***
+	 * Konstruktor
+	 */
 	public GameBoard() {
-		
+		dice = new Dice();
 	}
-	
-	public int getLastRoll() {
-		return lastRoll;
+
+	/***
+	 * Gibt die Instanz des Würfels zurück
+	 * 
+	 * @return
+	 */
+	public Dice getDice() {
+		return dice;
 	}
-	
+
 	/***
 	 * Gibt die Liste aller teilnehmenden Spieler zurück
+	 * 
 	 * @return
 	 */
 	public ArrayList<Player> getPlayers() {
@@ -27,15 +36,18 @@ public class GameBoard {
 
 	/***
 	 * Gibt die Liste alle Spielfelder zurück
+	 * 
 	 * @return
 	 */
 	public ArrayList<Field> getAllFields() {
 		return fields;
 	}
-	
+
 	/***
 	 * Fügt den angegebenen Spieler dem Spielfeld hinzu
-	 * @param player Der hinzuzufügende Spieler
+	 * 
+	 * @param player
+	 *            Der hinzuzufügende Spieler
 	 */
 	public void addPlayer(Player player) {
 		if (!players.contains(player)) {
@@ -45,18 +57,19 @@ public class GameBoard {
 
 	/***
 	 * Gibt den Spieler zurück, welcher aktuell an der Reihe ist
+	 * 
 	 * @return
 	 */
 	public Player getCurrentPlayer() {
 		return players.get(currentPlayer);
 	}
-	
+
 	/***
 	 * Beendet den Spielzug des aktuellen Spielers
 	 */
-	public void endMove(){
+	public void endMove() {
 		currentPlayer++;
-		if(currentPlayer > players.size() - 1)
+		if (currentPlayer > players.size() - 1)
 			currentPlayer = 0;
 	}
 
