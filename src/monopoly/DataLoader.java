b5902d10e55d1ef363bfad81889e6f3f24872f4f
jan-1234 <jan.e@online.de>
2013-11-11 10:18:06 +0100

    Set field position

--- src/monopoly/DataLoader.java
+++ src/monopoly/DataLoader.java
@@ -19,25 +19,29 @@ public class DataLoader {
 		ArrayList<Field> fields = new ArrayList<Field>();		
 		
 		ObjectMapper mapper = new ObjectMapper();
-		SerializedField[] rawFields = mapper.readValue(new File("data/fields.json"), SerializedField[].class);		
+		SerializedField[] rawFields = mapper.readValue(new File("data/fields.json"), SerializedField[].class);	
 		
-		for (SerializedField rawField : rawFields) {			
+		int position = 0;
+		for (SerializedField rawField : rawFields) {
+			PropertyData data = rawField.data;
+			data.setPosition(position);
 			if (rawField.className.equals("Street"))
-				fields.add(new Street(rawField.data));
+				fields.add(new Street(data));
 			else if (rawField.className.equals("CardStack"))
-				fields.add(new CardStack(rawField.data));
+				fields.add(new CardStack(data));
 			else if (rawField.className.equals("ActionField"))
-				fields.add(new ActionField(rawField.data));
+				fields.add(new ActionField(data));
 			else if (rawField.className.equals("Trainstation"))
-				fields.add(new TrainStation(rawField.data));
+				fields.add(new TrainStation(data));
 			else if (rawField.className.equals("SupplyStation"))
-				fields.add(new SupplyStation(rawField.data));
+				fields.add(new SupplyStation(data));
 			else if (rawField.className.equals("Prison"))
-				fields.add(new Prison(rawField.data));
+				fields.add(new Prison(data));
 			else if (rawField.className.equals("Field"))
-				fields.add(new Field(rawField.data));
+				fields.add(new Field(data));
 			else
 				throw new Exception("Unknown field type " + rawField.className);
+			position++;
 		}
 		
 		GameBoard gameBoard = new GameBoard(fields);
