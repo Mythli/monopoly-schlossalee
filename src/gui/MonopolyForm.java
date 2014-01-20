bdc2f693cbc1b824159221c880ead0814b3f3550
Lokaler Benutzer <Lokaler Benutzer@R-166-04.Schule.107842.mts-schule.de>
2014-01-20 10:26:20 +0100

    FormKlasse hinzugefügt

---
+++ src/gui/MonopolyForm.java
@@ -0,0 +1,85 @@
+
+package gui;
+
+import monopoly.*;
+
+
+
+public class MonopolyForm implements events.IDiceHandler, events.IPlayerMovementHandler, events.IAccountHandler, events.IPurchaseHandler{
+	
+	public MonopolyForm ()
+	{		
+	}
+	Monopoly monopoly = new Monopoly ();
+
+	@Override
+	public void diceRolled(int dice) {
+		System.out.println("Es wurde gewürfelt " + dice);
+	}
+
+	@Override
+	public void playerMoved(Player player, Field field) {
+		System.out.println("Spieler " + player.getName() + " muss auf das Feld " + field.property.getName());
+	}
+	
+	public void addPlayers ()
+	{
+		monopoly.getGameBoard().addPlayer(new Player("Fredde"));
+		monopoly.getGameBoard().addPlayer(new Player("Stefan"));
+		monopoly.getGameBoard().addPlayer(new Player("Jan"));
+	}
+	
+	public void start ()
+	{
+		addPlayers();
+		for(int zaehler = 1; zaehler < 20; zaehler++ )
+		{
+			
+			try {
+				monopoly.getGameBoard().makeMove();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
+			
+			System.out.println("Spieler steht aktuell auf Feld " + monopoly.getGameBoard().getCurrentPlayer().getCurrentField().property.getName());
+			
+			try {
+				monopoly.getGameBoard().getCurrentPlayer().buyField();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
+			
+		}
+		
+	}
+
+	@Override
+	public void balanceChanged(Player player) {
+		//Kontostand hat sich geändert
+		
+		System.out.println("Der Kontostand für Spieler "+ player.getName() + "hat sich geändernt");
+		System.out.println("Der Kontostand hat sich auf " + player.getCurrentMoney() + " geändert!");
+		
+	}
+
+	@Override
+	public void playerBoughtHouse(Player player, PropertyField field) {
+		System.out.println("Der Spieler "+ player.getName()+ " hat ein House gekauft");
+		
+	}
+
+	@Override
+	public void playerBoughtHotel(Player player, PropertyField field) {
+		System.out.println("Der Spieler "+ player.getName()+ " hat ein Hotel gekauft");
+		//monopoly.getGameBoard().getCurrentPlayer().buyHotel();
+		
+	}
+	
+	
+	
+	
+	
+
+}
